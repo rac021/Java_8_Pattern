fc1655eccf8f386a16559e6d19a470507d4edeb5
Rachid Yahiaoui <ryahiaoui@ORL-5CB3410DK4.orleans.inra.fr>
2016-05-03 16:30:03 +0200

    Around Method Pattern

---
+++ Arround_Method_Pattern/src/main/java/org/inra/around_method_pattern/Resource_01_W_TRY.java
@@ -0,0 +1,31 @@
+
+package org.inra.around_method_pattern;
+
+/**
+ *
+ * @author ryahiaoui
+ */
+
+class Resource_TRY { 
+    
+    public Resource_TRY()  { System.out.println(" Get Connection.... ") ; } 
+    public void opOne()    { System.out.println(" Run Op One ")         ; } 
+    public void opTwo()    { System.out.println(" Run Op Two ")         ; }    
+    public void finalize() { System.out.println(" CleanUp Connection ") ; }
+}
+
+ public class Resource_01_W_TRY {
+
+     public static void main(String[] args) {
+         Resource_TRY resource = new Resource_TRY() ;
+        try {
+          resource.opOne()    ;
+          resource.opTwo()    ;
+        }
+        finally {
+          resource.finalize() ;
+        }
+        // Easy to forget ++ ( Try and CLose )
+     }
+   
+}
