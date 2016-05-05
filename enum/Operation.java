7c699e55ee79cfe478d677ee4bf05de2e0aa11fc
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-05 04:00:19 +0200

    Update Operation.java

--- enum/Operation.java
+++ enum/Operation.java
@@ -5,7 +5,7 @@ public enum Operation {
     
   PLUS   { double eval ( double x, double y ) { return x + y ; } } ,
   MINUS  { double eval ( double x, double y ) { return x - y ; } } ,
-  TIMES  { double eval ( double x, double y ) { return x * y ; } } ,
+  MULT   { double eval ( double x, double y ) { return x * y ; } } ,
   DIVIDE { double eval ( double x, double y ) { return x / y ; } } ;
 
   // Arithmetic op represented by this constant
