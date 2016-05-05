c27f6e5c12e9df822c5d12c6d8b62f4382198be5
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-05 03:54:35 +0200

    Update Operation.java

--- enum/Operation.java
+++ enum/Operation.java
@@ -3,19 +3,17 @@ package com.inra.enumeration ;
 
 public enum Operation {
     
-  PLUS   { double eval ( double x, double y ) { return x + y ; } } ,
-  MINUS  { double eval ( double x, double y ) { return x - y ; } } ,
-  TIMES  { double eval ( double x, double y ) { return x * y ; } } ,
-  DIVIDE { double eval ( double x, double y ) { return x / y ; } } ;
+    PLUS   { double eval ( double x, double y ) { return x + y ; } } ,
+    MINUS  { double eval ( double x, double y ) { return x - y ; } } ,
+    TIMES  { double eval ( double x, double y ) { return x * y ; } } ,
+    DIVIDE { double eval ( double x, double y ) { return x / y ; } } ;
 
-  // Arithmetic op represented by this constant
-  abstract double eval(double x, double y) ;
+    // Arithmetic op represented by this constant
+    abstract double eval(double x, double y) ;
   
   public static void main ( String[] args )  {
-  
     Operation op = Operation.PLUS       ;
     double   sum = op.eval( 21 , 21 )   ;
     System.out.println("sum = " + sum)  ;
-
   }
 }
