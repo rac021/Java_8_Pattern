187143318b349a7a45e7b9b2ce1e852b3589c562
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-07 03:00:36 +0200

    Create LoggerWLevel.java

---
+++ Logger/LoggerWLevel.java
@@ -0,0 +1,39 @@
+
+package com.rac.visitorlambda;
+
+/**
+ *
+ * @author Rac021
+ */
+
+import java.util.Objects ;
+import java.util.function.Predicate ;
+
+public class HelloWorld {
+
+     public static void main(String []args) {
+  
+       LoggerLevel loggerLevel = ( level, message ) -> System.out.println( level + " --- " + message ) ;
+       
+       Logger loggerError  = loggerLevel.level(Level.ERROR);
+       Logger loggerWarn   = loggerLevel.level(Level.WARN) ;
+      
+       loggerError.log("Disk quota exceeded") ; 
+       loggerWarn.log("Low disk space") ; 
+     }
+
+    enum Level {  WARN , ERROR ;  }
+    
+    interface LoggerLevel {
+      void log ( Level level , String message ) ;
+      
+      default Logger level ( Level level ) {
+          return message -> log ( level, message) ;
+      }
+    }
+    
+    interface Logger {
+        void log( String message ) ;
+    }
+
+}
