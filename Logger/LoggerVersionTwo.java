30afb506dd7584a7bfb5dfca33da89ed7cc19fca
Yahiaoui Rachid <rac021@hotmail.com>
2016-05-07 02:25:44 +0200

    Create LoggerVersionTwo.java

---
+++ Logger/LoggerVersionTwo.java
@@ -0,0 +1,37 @@
+
+package org.inra.logger ;
+
+/**
+ *
+ * @author Rac021
+ */
+import java.util.Objects ;
+import java.util.function.Predicate ;
+
+public class HelloWorld {
+
+     public static void main(String []args) {
+  
+       Logger logger        =  msg -> System.out.println(msg);
+       Logger loggerFilter  =  logger.filter ( msg -> msg.contains("Error") ) ;
+       
+       loggerFilter.log("Error : disk quota exceeded ") ; 
+       
+       loggerFilter.log("WARN : low disk space ") ; 
+     }
+
+
+    interface Logger {
+        
+        void log( String message ) ;
+        
+        default Logger filter ( Predicate<String> filter ) {
+            Objects.requireNonNull(filter );
+            return 
+                msg -> { 
+                    if( filter.test(msg) ) { log(msg) ; }
+                } ;
+        }
+    }
+
+}
